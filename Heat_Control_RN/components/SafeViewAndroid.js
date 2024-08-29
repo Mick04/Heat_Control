8c2eb4f8cea253ec80a366ddff1e17e1f3e1415a
Michael Cave <michaelcave@Michaels-Mac-mini-Pro.local>
2024-08-29 15:48:39 +0100

    cleared old folders out

--- Heat_Control_RN/components/SafeViewAndroid.js
+++
@@ -1,9 +0,0 @@
-import { StyleSheet, Platform} from "react-native";
-
-export default StyleSheet.create({
-  AndroidSafeArea: {
-    flex: 1,
-    backgroundColor: "white",
-    paddingTop: Platform.OS === "android" ? 250 : 0
-  }
-}); 
